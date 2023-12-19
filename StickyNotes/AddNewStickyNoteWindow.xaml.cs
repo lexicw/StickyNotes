53487a59b611a52dc307473e886aeea2b1976111
Alexandra Wheeler <acw@siteenhancementservices.com>
2023-12-19 16:47:49 -0500

    Modifications to sticky notes grid, etc.

--- StickyNotes/AddNewStickyNoteWindow.xaml.cs
+++ StickyNotes/AddNewStickyNoteWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,15 +29,43 @@ namespace StickyNotes
             ViewModel = stickyNoteViewModel;
             DataContext = ViewModel;
 
+            ColorStackPanel.DataContext = this;
+
             Owner = Application.Current.MainWindow;
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            ViewModel.Color = "#FFFFFF";
         }
 
         public StickyNoteViewModel ViewModel { get; }
 
+
         private void AddNewNoteButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
+        }
+
+        private void ColorRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            var checkedValue = ColorStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value);
+            var color = checkedValue.Background;
+
+            ViewModel.Color = color.ToString();
+        }
+
+    }
+
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class EqualityToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return parameter?.Equals(value) ?? false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
         }
     }
 
--- StickyNotes/StickyNoteViewModel.cs
+++ StickyNotes/StickyNoteViewModel.cs
@@ -58,48 +58,6 @@ namespace StickyNotes
             }
         }
 
-        public void AddDummyNote()
-        {
-            StickyNote = new StickyNote
-            {
-                Date = DateTime.Now,
-                Text = GetRandomText(),
-                Color = GetRandomColor()
-            };
-        }
-
-        private string GetRandomText()
-        {
-            string[] availableTexts = new string[] {
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "Lorem ipsum dolor sit amet."
-    };
-            Random rnd = new Random();
-            return availableTexts[rnd.Next(availableTexts.Length)];
-        }
-
-        private string GetRandomColor()
-        {
-            string[] availableColors = new string[] { "#e4f0ea", "#f3d8da", "#fdf3d1", "White" };
-            Random rnd = new Random();
-            return availableColors[rnd.Next(availableColors.Length)];
-        }
-
-
-        //public void AddNewNote()
-        //{
-        //    StickyNoteViewModel newNote = new StickyNoteViewModel();
-
-        //    newNote.Color = "Yellow";
-        //    newNote.Date = DateTime.Now;
-        //    newNote.Text = "This is a new note";
-
-        //    // Add the new note view model to the list
-        //    StickyNotesList.Add(newNote);
-        //}
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
